e2a408140f75b701def348fdcb32c808c9610f97
Srinath D <srinath.dussa@gmail.com>
2017-04-26 23:43:15 +0530

    moved the files to parent dir

---
+++ server.js
@@ -0,0 +1,55 @@
+const express=require('express');
+const app = express();
+const bodyparser = require('body-parser');
+
+var dbReference = require('./modules/dbWrapper')
+var dbWrapper = dbReference({});
+
+console.log('sample hI1');
+
+app.use(bodyparser.urlencoded({ extended: true }))
+
+app.get('/', function (req, res) {
+    //res.send('Hello');
+    res.sendFile(__dirname + '/Home.html');
+});
+
+app.post('/quotes', (req, res) => {
+    console.log(req.body);
+
+    var callBack = (err, response) => {
+        res.send('done');
+    }
+
+    dbWrapper.addDataToCollection({
+        collectionName: 'quotes',
+        data: req.body
+    }, callBack);
+});
+
+app.get('/getquotes', (req, res) => {
+    console.log(req.body);
+
+    var callBack = (err, response) => {
+        res.json(response);
+    }
+
+    dbWrapper.findfromCollections({
+        collectionName: 'quotes',
+        data: req.body
+    }, callBack);
+})
+
+dbWrapper.connectToDb((connected) => {
+    if (connected) {
+        app.listen(3000, function () {
+            console.log('listening on 3000');
+        })
+    }
+    else {
+        console.log('cannot continue without connecting to Db');
+    }
+}
+)
+
+//https://zellwk.com/blog/crud-express-mongodb/
